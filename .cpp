3069fe6fd73cc8d5c61e4a8f5020b6a9055fdacc
Rixard <78233871+NotRixard@users.noreply.github.com>
2025-02-09 23:03:17 +0100

    Update .cpp

--- .cpp
+++ .cpp
@@ -1,32 +1,11 @@
-# Prerequisites
-*.d
+#include <iostream>
+using namespace std;
 
-# Compiled Object files
-*.slo
-*.lo
-*.o
-*.obj
-
-# Precompiled Headers
-*.gch
-*.pch
-
-# Compiled Dynamic libraries
-*.so
-*.dylib
-*.dll
-
-# Fortran module files
-*.mod
-*.smod
-
-# Compiled Static libraries
-*.lai
-*.la
-*.a
-*.lib
-
-# Executables
-*.exe
-*.out
-*.app
+int main() {
+    int a;
+    cout << "Write a number: ";
+    cin >> a;
+    int sum = a * a;
+    cout << "The area of the square is: " << sum;
+return 0;
+}
